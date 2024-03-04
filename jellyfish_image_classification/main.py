0985a65a7545e13ac6dcd7ba2051d2c11b4b3096
PeterOstr <61982891+PeterOstr@users.noreply.github.com>
2024-03-05 02:21:20 +0300

    api training added

--- jellyfish_image_classification/main.py
+++ jellyfish_image_classification/main.py
@@ -1,16 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import pandas as pd
 import numpy as np
 from pydantic import BaseModel
 from typing import List
 from fastapi.staticfiles import StaticFiles
-from sklearn.metrics import f1_score, roc_auc_score
+from keras.models import load_model
+import tensorflow as tf
+from PIL import Image
 
-
-import joblib
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+class ImageData(BaseModel):
+    image_file: UploadFile
+
+jelly_type_df = pd.DataFrame({
+    0: ['Moon_jellyfish'],
+    1: ['barrel_jellyfish'],
+    2: ['blue_jellyfish'],
+    3: ['compass_jellyfish'],
+    4: ['lions_mane_jellyfish'],
+    5: ['mauve_stinger_jellyfish']
+}).T
+
+model = load_model('C:/Users/Peter/DataspellProjects/kaggle/jellyfish_image_classification/model_new.h5')
+
+@app.post("/model/predict")
+async def predict(data: ImageData):
+    contents = await data.image_file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image_resized = image.resize((224, 224))
+
+    image_tensor = tf.keras.preprocessing.image.img_to_array(image_resized)
+    image_tensor = np.expand_dims(image_tensor, axis=0)  # Добавляем измерение пакета
+
+    result = model.predict(image_tensor)
+    max_index = np.argmax(result)
+
+    predicted_jellyfish_type = jelly_type_df.iloc[max_index, 0]
+
+    return predicted_jellyfish_type
